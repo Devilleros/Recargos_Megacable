3bf9cedde8b1d68edeb4bca605518a717c1fb5f0
Devilleros <102046673+Devilleros@users.noreply.github.com>
2022-04-01 16:57:20 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function Recargos() {
     var n2 = document.getElementById('num2');
     var n3 = document.getElementById('num3');
     var n4 = document.getElementById('num4');
+    /*var n4 = document.getElementById('num4');
     var n5 = document.getElementById('num5');
     console.log(n5);
     console.log(5);
@@ -12,9 +13,16 @@ function Recargos() {
         console.log("??");
         return true;
     }
+    */
     
-    
-    alert(55);
+    let hourN = parseFloat(n1.value)/30/8;
+    let hourD = hourN * 1.75;
+    let dN = parseFloat(n2.value)*9.5;
+    let dD = parseFloat(n3.value)*9.5
+    let extra = parseFloat(n2.value) * 0.25 * hourN; 
+    let x = (hourD * dD) + (hourN * dN);
+    let y = x - (parseFloat(n1.value)*0.08) + 58586 + extra;
+    alert(parseInt(y));
 
     return false;
 }
