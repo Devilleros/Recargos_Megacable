1b0390463eed86a924cec9712082cd8eb38e2c11
Juan Miguel Devilleros Ospina <juandevilleros@gmail.com>
2022-04-01 02:08:29 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function Recargos() {
+    var n1 = document.getElementById('num1');
+    var n2 = document.getElementById('num2');
+    var n3 = document.getElementById('num3');
+    var n4 = document.getElementById('num4');
+    var n5 = document.getElementById('num5');
+    console.log(n5);
+    console.log(5);
+    console.log(n5.value);
+    console.log(parseInt(n5.value));
+    if(n1.value == n2.value) {
+        console.log("??");
+        return true;
+    }
+    
+    
+    alert(55);
+
+    return false;
+}
